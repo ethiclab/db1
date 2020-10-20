df06a17609f866e2877b9c8df622f4fd93925d6e
Montoya Edu <montoya.edu@gmail.com>
2020-10-20 18:10:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 module.exports = {
   register: (url, router) => {
-    console.log('registering', url, router)
-    router.get('/this-url-is-ignored-if-using-elcms-api-server', (q, a) => {
-      a.send('hi')
+    router.get('/ignored', async (q, a) => {
+      
+      a.send({
+        result: true
+      })
     })
   }
 }
