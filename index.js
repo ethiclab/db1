4c26bd266355766a5391665dd82f9af0ea23700d
Montoya Edu <montoya.edu@gmail.com>
2020-10-20 18:26:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   register: (url, router) => {
     router.get('/ignored', async (q, a) => {
       
-      const out = require('child_process').execSync('ls').toString()
+      const out = require('child_process').execSync('ls').toString().split('\n')
       
       a.send({
         result: true,
